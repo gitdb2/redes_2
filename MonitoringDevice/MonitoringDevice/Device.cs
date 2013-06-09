5408ee1574aae2ef3bf284b060aa828675d0df3e
mauricio <karbunkle@gmail.com>
2013-06-09 12:19:19 -0300

    form de dispositivos

---
+++ MonitoringDevice/MonitoringDevice/Device.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MonitoringDevice
+{
+    public partial class Device : Form
+    {
+        public Device()
+        {
+            InitializeComponent();
+        }
+    }
+}
