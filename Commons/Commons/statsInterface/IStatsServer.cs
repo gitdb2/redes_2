59869dd53ad6c02a044f6e610cde2f9abba5f798
Rodrigo <rodrigodemi@gmail.com>
2013-06-14 18:55:33 -0300

    interface para obener los datos de un dispositivo

---
+++ Commons/Commons/statsInterface/IStatsServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace uy.edu.ort.obligatorio.Commons.statsInterface
+{
+    interface IStatsServer
+    {
+        public List<DeviceFailureInfo> GetDeviceFaults(string idDevice, int resultMaxSize);
+        public List<DeviceStatusInfo> GetDeviceFaults(string idDevice, int resultMaxSize);
+    }
+}
